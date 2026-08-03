4e2116d6d8902df4deef7605ff0633234dd6e33b
Faiq Sukurlu <sukurlufaiq3521@gmail.com>
2026-08-03 11:58:02 +0400

    Initial commit - environment setup

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import folium
+from ortools.constraint_solver import pywrapcp
+
+print("Hər şey işləyir! Pandas versiya:", pd.__version__)
+print("OR-Tools uğurla import edildi.")
